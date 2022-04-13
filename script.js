e97fc01fd5b5586123d720943e651f801a9d5232
matiasagnese <matiasagnese@gmail.com>
2022-04-13 20:17:03 -0300

    primera version

---
+++ script.js
@@ -0,0 +1,64 @@
+// alert("Hola mundo")
+//
+
+//console.log("Hola consola")
+
+//console.warn("Hola consola")
+
+//console.error("Hola consola")
+//let, var y const es que estoy definiendo una variable
+let alumna 
+
+alumna = "matias"
+
+let edad = 21
+
+console.log( "La alumna que ingreso es " + alumna + " y la edad es " + edad);
+//camel case
+let nombreApellido = " jeimu paurl"
+//snake case
+let nombre_apellido = "juan carlos"
+//pascal Case
+let NombreApellido = "fede suarez"
+let $nombreApellido
+let _nombreApellido
+
+//no vannnn
+//let nombre-apellido = "juan carlos" //kebab case
+
+const entidad = "gob ciudad " //el const no se puede pisar, es constante
+
+console.log("la entidad es " + entidad)
+
+////////////TIPOS DE DATOS//////////////////
+//string
+
+let str1 = "un string"
+let str2 = 'otro string'
+//let str3 = ´otro string mas´
+
+let str4 = str1 + " y " + str2 + " se casan "
+console.log(str4)
+//let str5 = ${str1} y ${str2} se casan
+//console.log(str5)
+
+let nombreVisitante = prompt("Hola, como te llamas")
+
+console.log(nombreVisitante)
+
+///////////////NUMBER
+let numero1 = parseFloat(prompt("mete el primer numero"))
+
+let numero2 = parseFloat(prompt("mete el segundo numero"))
+
+
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multi = numero1 * numero2
+let division = numero1/numero2
+
+console.log("La suma es " + suma)
+console.log("La resta es " + resta)
+console.log("La multi es " + multi)
+console.log("La division es " + division)
+
